17560edd5fbf6d87510504ffbd11c613595f8550
nathanrydel <nathanrydel@gmail.com>
2023-03-16 22:59:31 -0700

    add challenge ROE

--- Functions and Execution Context/controlFlow-ifStatementsandMathObj.js
+++ Functions and Execution Context/controlFlow-ifStatementsandMathObj.js
@@ -1,3 +1,7 @@
+// Using an IF/ELSE statement, write a function closestToTheMark that takes two player inputs as number arguments. The function will return 'Player 1 is closest' or 'Player 2 is closest' depending on which player input is closest to the randomly generated number.
+
+// Note: Due to the output being based off of a random factor, the tests provided below cannot determine if you have solved the prompt exactly. The test cases for this challenge check for the correct data types only.
+
 function closestToTheMark(player1, player2) {
   const theMark = Math.floor(Math.random() * 100);
   console.log(`If theMark is ${theMark}...`);
