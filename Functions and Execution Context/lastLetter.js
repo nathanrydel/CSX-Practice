e3eaaee44f0dd31006329c46bc459d8470ee4329
Nathan Rydel <nathanrydel@gmail.com>
2023-03-18 22:30:51 -0700

    Challenge: lastLetter

---
+++ Functions and Execution Context/lastLetter.js
@@ -0,0 +1,10 @@
+function lastLetter(word) {
+  // INSERT CODE HERE
+  return word.at(-1);
+}
+
+// Uncomment the lines below to test your code
+console.log(lastLetter('hello')); //=> "o"
+console.log(lastLetter('goodbye!')); //=> "!"
+console.log(lastLetter('ZeltoiD')); //=> "D"
+console.log(lastLetter('I love Javascript')); //=> "t"
