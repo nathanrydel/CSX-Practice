5a83e4b94697fc28ed106e668ca290f9dc6eb4e0
Nathan Rydel <nathanrydel@gmail.com>
2023-03-18 22:34:47 -0700

    Challenge: isOdd

---
+++ Functions and Execution Context/isOdd.js
@@ -0,0 +1,11 @@
+// ADD CODE HERE
+function isOdd(num) {
+  if (num % 2 === 1) {
+    return true;
+  } else {
+    return false;
+  }
+}
+// Uncomment these to check your work!
+console.log(isOdd(5)); // should log: true
+console.log(isOdd(2008)); // should log: false
