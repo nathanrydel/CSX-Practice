5bad54191b755728e5e2d8bfd6d3db809ab4d9bd
Nathan Rydel <nathanrydel@gmail.com>
2023-03-18 22:29:31 -0700

    Challenge: sayHello

---
+++ Functions and Execution Context/sayHello.js
@@ -0,0 +1,5 @@
+// ADD CODE HERE
+let sayHello = (firstName) => 'Hi, ' + firstName;
+// Uncomment these to check your work!
+console.log(sayHello('Mary')); // should log: 'Hi, Mary'
+console.log(sayHello('Haley')); // should log: 'Hi, Haley'
