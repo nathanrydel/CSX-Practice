1ae85b899b1c44c065afb9d9b50d1c04e5321444
Nathan Rydel <nathanrydel@gmail.com>
2023-03-18 22:43:37 -0700

    follow ES6 bp for arrows

--- Functions and Execution Context/sayHello.js
+++ Functions and Execution Context/sayHello.js
@@ -1,5 +1,5 @@
 // ADD CODE HERE
-let sayHello = (firstName) => 'Hi, ' + firstName;
+const sayHello = (firstName) => 'Hi, ' + firstName;
 // Uncomment these to check your work!
 console.log(sayHello('Mary')); // should log: 'Hi, Mary'
 console.log(sayHello('Haley')); // should log: 'Hi, Haley'
