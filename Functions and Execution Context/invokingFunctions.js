7f1be92f99b8672f8341c6442ad9d48309f41995
Nathan Rydel <nathanrydel@gmail.com>
2023-03-18 22:48:08 -0700

    update to arrows

--- Functions and Execution Context/invokingFunctions.js
+++ Functions and Execution Context/invokingFunctions.js
@@ -1,33 +1,33 @@
 let calls = '';
 
 // output needs to be JerryKramerGeorgeElaine
-function jerry(str) {
+const jerry = (str) => {
   //concat str with "Jerry"
   str += 'Jerry';
   //return invoc of kramer and pass str
   return kramer(str);
-}
+};
 
-function george(str) {
+const george = (str) => {
   // concat str with "George"
   str += 'George';
   // return invoc of elaine and pass str
   return elaine(str);
-}
+};
 
-function elaine(str) {
+const elaine = (str) => {
   // concat str with "Elaine"
   str += 'Elaine';
   // return str
   return str;
-}
+};
 
-function kramer(str) {
+const kramer = (str) => {
   // concat str with "Kramer"
   str += 'Kramer';
   // return in invoc of george
   return george(str);
-}
+};
 
 // Uncomment these to check your work!
 calls = jerry(calls);
