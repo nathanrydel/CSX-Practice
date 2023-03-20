ce26b96487872cbaf5df2fc2690ec08430f45a6f
Nathan Rydel <nathanrydel@gmail.com>
2023-03-20 10:40:19 -0700

    Challenge: Disemvowel

---
+++ Functions and Execution Context/disemvowel.js
@@ -0,0 +1,9 @@
+const disemvowel = (string) => {
+  // iterate thru string with FOR loop
+  // check if
+};
+
+// Uncomment these to check your work!
+// console.log(disemvowel('CodeSmith')); // => 'CdSmth'
+// console.log(disemvowel('BANANA')); // => 'BNN'
+// console.log(disemvowel('hello world')); // => 'hll wrld'
