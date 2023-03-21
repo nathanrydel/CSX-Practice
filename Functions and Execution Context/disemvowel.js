68cf6e9d65c669fa5531a3854abb1ec0ea18664d
Nathan Rydel <nathanrydel@gmail.com>
2023-03-21 13:22:53 -0700

    Challenge: disemvowel

--- Functions and Execution Context/disemvowel.js
+++ Functions and Execution Context/disemvowel.js
@@ -1,9 +1,30 @@
-const disemvowel = (string) => {
-  // iterate thru string with FOR loop
-  // check if
+const disemvowel = (str) => {
+  // delcare a result var to store output
+  let rslt = '';
+  // iterate thru str with FOR loop
+  for (let i = 0; i < str.length; i++) {
+    // assign str at element to variable char
+    // necessary to later convert to lowercase
+    let char = str[i];
+    // check if letter is a not vowel - need a long chain of && and !==
+    // needs to check both upper and lowercase
+    // using the .toLowerCase() method
+    if (
+      char.toLowerCase() !== 'a' &&
+      char.toLowerCase() !== 'e' &&
+      char.toLowerCase() !== 'i' &&
+      char.toLowerCase() !== 'o' &&
+      char.toLowerCase() !== 'u'
+    ) {
+      // add characters that are not vowels to rslt
+      rslt += char;
+    }
+  }
+  // return ouput (rslt)
+  return rslt;
 };
 
 // Uncomment these to check your work!
-// console.log(disemvowel('CodeSmith')); // => 'CdSmth'
-// console.log(disemvowel('BANANA')); // => 'BNN'
-// console.log(disemvowel('hello world')); // => 'hll wrld'
+console.log(disemvowel('CodeSmith')); // => 'CdSmth'
+console.log(disemvowel('BANANA')); // => 'BNN'
+console.log(disemvowel('hello world')); // => 'hll wrld'
