a1a2ae27754827202d7f9f5d91f174a628720e81
Nathan Rydel <nathanrydel@gmail.com>
2023-03-23 12:06:40 -0700

    Providing challenge text

--- Functions and Execution Context/arrayBuilder.js
+++ Functions and Execution Context/arrayBuilder.js
@@ -1,3 +1,5 @@
+// Write a function arrayBuilder that takes in a count object and returns an array filled with the appropriate numbers of elements. The order of the elements in the array does not matter, but repeated elements should be grouped.
+
 const arrayBuilder = (obj) => {
   // declare empty array, resultArray to store result
   const resultArray = [];
