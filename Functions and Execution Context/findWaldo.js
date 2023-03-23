8d494156ab171c782a8c8c53206bad3350b38350
Nathan Rydel <nathanrydel@gmail.com>
2023-03-22 19:12:09 -0700

    Challenge: findWaldo === Completed

--- Functions and Execution Context/findWaldo.js
+++ Functions and Execution Context/findWaldo.js
@@ -1,10 +1,17 @@
 // Create a function findWaldo that accepts an object and returns the value associated with the key 'Waldo'. If the key 'Waldo' is not found, the function should return 'Where's Waldo?'
 
-const findWaldo = (waldoObj) => {
-  waldoObj[]
-}
+const findWaldo = (obj) => {
+  // iterate through obj
+  for (let key in obj) {
+    // check if key is "Waldo"
+    if (key === "Waldo") {
+      return obj[key];
+    }
+  }
+  return "Where's Waldo?";
+};
 
-// const DC = {'Bruce': 'Wayne', 'Harley': 'Quinn'}
-// const supernatural = {'Sam': 'Winchester', 'Dean': 'Winchester', 'Waldo': 'unknown'}
-// console.log(findWaldo(DC)) // should log: 'Where's Waldo?'
-// console.log(findWaldo(supernatural)) // should log: 'unknown'
+const DC = { Bruce: "Wayne", Harley: "Quinn" };
+const supernatural = { Sam: "Winchester", Dean: "Winchester", Waldo: "unknown" };
+console.log(findWaldo(DC)); // should log: 'Where's Waldo?'
+console.log(findWaldo(supernatural)); // should log: 'unknown'
