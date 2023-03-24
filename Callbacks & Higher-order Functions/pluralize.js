aa496952569f30f3453dcd99f3f96766aeb3f3c8
Nathan Rydel <nathanrydel@gmail.com>
2023-03-23 18:27:58 -0700

    Challenge: pluralize completed

--- Callbacks & Higher-order Functions/pluralize.js
+++ Callbacks & Higher-order Functions/pluralize.js
@@ -2,6 +2,17 @@
 
 // The body of the pluralize function should employ a single for loop that pluralizes each string in the input array.
 
+// declare func pluralize that takes an array of strings as a param
+const pluralize = (array) => {
+  // assign output as an empty array
+  let output = [];
+  // loop thru array and add an s to the indiviual elements
+  for (let i = 0; i < array.length; i++) {
+    output.push(array[i] + "s");
+  }
+  return output;
+};
+
 // Uncomment these to check your work!
-// const animals = ["dog", "cat", "tree frog"];
-// console.log(pluralize(animals)); // should log: ["dogs", "cats", "tree frogs"]
+const animals = ["dog", "cat", "tree frog"];
+console.log(pluralize(animals)); // should log: ["dogs", "cats", "tree frogs"]
