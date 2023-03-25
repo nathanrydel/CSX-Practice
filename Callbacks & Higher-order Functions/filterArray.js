a7a4b9a3299d7319e0008f94a1f5eaca0da64a6a
nathanrydel <nathanrydel@gmail.com>
2023-03-24 22:10:45 -0700

    filterArray Changes

---
+++ Callbacks & Higher-order Functions/filterArray.js
@@ -0,0 +1,28 @@
+// Add code to the functions func1 and func2 in the places marked "ADD CODE HERE" in order to achieve the desired console logs.
+
+const filterArray = (array, callback) => {
+  const newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (callback(array[i])) newArray.push(array[i]);
+  }
+  return newArray;
+};
+
+const arrOfNums = [1, 2, 3, 4, 5];
+const func1 = (num) => {
+  // ADD CODE HERE
+  const evenArray = [];
+  filterArray(num, function (element) {
+    if (element % 2 === 0) {
+      evenArray.push(element);
+    }
+  });
+};
+
+function func2(num) {
+  // ADD CODE HERE
+}
+
+// Uncomment these to check your work!
+console.log(filterArray(arrOfNums, func1)); // should log: [2, 4]
+// console.log(filterArray(arrOfNums, func2)); // should log: [1, 3, 5]
