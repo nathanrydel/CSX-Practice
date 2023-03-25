b3e6c2d3881ef798d68ca5efd706fd13624a4c69
nathanrydel <nathanrydel@gmail.com>
2023-03-24 22:12:36 -0700

    issue

--- Callbacks & Higher-order Functions/filterArray.js
+++ Callbacks & Higher-order Functions/filterArray.js
@@ -24,5 +24,5 @@ function func2(num) {
 }
 
 // Uncomment these to check your work!
-console.log(filterArray(arrOfNums, func1)); // should log: [2, 4]
+console.log(filterArray(arrOfNums, func1)); // should log: [2, 4] but currently logging empty array
 // console.log(filterArray(arrOfNums, func2)); // should log: [1, 3, 5]
