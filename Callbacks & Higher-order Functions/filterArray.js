d5c39007ff5ec9a1b738f353ca1e5a38e81f0840
nathanrydel <nathanrydel@gmail.com>
2023-03-28 18:20:21 -0700

    Challenge: filterArray === completed

--- Callbacks & Higher-order Functions/filterArray.js
+++ Callbacks & Higher-order Functions/filterArray.js
@@ -11,18 +11,24 @@ const filterArray = (array, callback) => {
 const arrOfNums = [1, 2, 3, 4, 5];
 const func1 = (num) => {
   // ADD CODE HERE
-  const evenArray = [];
-  filterArray(num, function (element) {
-    if (element % 2 === 0) {
-      evenArray.push(element);
-    }
-  });
+  // check if numb is even
+  if (num % 2 === 0) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 function func2(num) {
   // ADD CODE HERE
+  // check if num is odd
+  if (num % 2 !== 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 // Uncomment these to check your work!
 console.log(filterArray(arrOfNums, func1)); // should log: [2, 4] but currently logging empty array
-// console.log(filterArray(arrOfNums, func2)); // should log: [1, 3, 5]
+console.log(filterArray(arrOfNums, func2)); // should log: [1, 3, 5]
