db3f2976aa2b502617ec780ccb85c021a94308f8
Nathan Rydel <nathanrydel@gmail.com>
2023-03-29 11:08:36 -0700

    Challenge: intersection === complete

--- Callbacks & Higher-order Functions/intersection.js
+++ Callbacks & Higher-order Functions/intersection.js
@@ -1,9 +1,33 @@
 // Construct a function intersection that compares input arrays and returns a new array with elements found in all of the inputs. BONUS - Use reduce!
 
-// ADD CODE HERE
+// input: array of arrays
+// output: array
+
+// declare intersection that takes arrays
+const intersection = (arrays) => {
+  // accumulator = starts as one of our arrays
+  let accumulator = arrays.pop();
+  // loop thru array of arrays
+  while (arrays.length) {
+    const current = arrays.pop();
+    // declare newArray to store comparisons
+    const newArray = [];
+    // compare current array with accumulator
+    // for each element in current
+    current.forEach((element) => {
+      // if that element exists in accumulator
+      // push element into a new array
+      if (accumulator.includes(element)) newArray.push(element);
+    });
+    // accumulator reassigned to new array
+    accumulator = newArray;
+  }
+  // return accumulator
+  return accumulator;
+};
 
 // Uncomment these to check your work!
-// const arr1 = [5, 10, 15, 20];
-// const arr2 = [15, 88, 1, 5, 7];
-// const arr3 = [1, 10, 15, 5, 20];
-// console.log(intersection([arr1, arr2, arr3])); // should log: [5, 15]
+const arr1 = [5, 10, 15, 20];
+const arr2 = [15, 88, 1, 5, 7];
+const arr3 = [1, 10, 15, 5, 20];
+console.log(intersection([arr1, arr2, arr3])); // should log: [5, 15]
