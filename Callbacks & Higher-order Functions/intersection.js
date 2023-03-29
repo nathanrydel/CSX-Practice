d0ef001db9d39a7d7c0105e742f9b64670701d67
Nathan Rydel <nathanrydel@gmail.com>
2023-03-28 20:52:01 -0700

    Challenge ROEs

---
+++ Callbacks & Higher-order Functions/intersection.js
@@ -0,0 +1,9 @@
+// Construct a function intersection that compares input arrays and returns a new array with elements found in all of the inputs. BONUS - Use reduce!
+
+// ADD CODE HERE
+
+// Uncomment these to check your work!
+// const arr1 = [5, 10, 15, 20];
+// const arr2 = [15, 88, 1, 5, 7];
+// const arr3 = [1, 10, 15, 5, 20];
+// console.log(intersection([arr1, arr2, arr3])); // should log: [5, 15]
---
+++ Callbacks & Higher-order Functions/majority.js
@@ -0,0 +1,8 @@
+// Create a function majority that accepts an array and a callback. The callback will return either true or false. majority will iterate through the array and perform the callback on each element until it can be determined if the majority of the return values from the callback are true. If the number of true returns is equal to the number of false returns, majority should return false.
+
+// ADD CODE HERE
+
+// Uncomment these to check your work!
+// const isOdd = function(num) { return num % 2 === 1; };
+// console.log(majority([1, 2, 3, 4, 5], isOdd)); // should log: true
+// console.log(majority([2, 3, 4, 5], isOdd)); // should log: false
