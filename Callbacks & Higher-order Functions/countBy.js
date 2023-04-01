7c2e3e921e23f9e951e7901e7221268c25d1928e
nathanrydel <nathanrydel@gmail.com>
2023-03-31 23:32:28 -0700

    countBy === complete

--- Callbacks & Higher-order Functions/countBy.js
+++ Callbacks & Higher-order Functions/countBy.js
@@ -2,10 +2,28 @@
 
 // ADD CODE HERE
 
+const countBy = (array, callback) => {
+  const countedArrayToObj = {};
+  // iterate thru array
+  for (let i = 0; i < array.length; i++) {
+    // declare key and set to callback at element of array
+    const key = callback(array[i]);
+    // check if key true
+    if (countedArrayToObj[key]) {
+      // increment key
+      countedArrayToObj[key]++;
+    } else {
+      // set key to 1 if false
+      countedArrayToObj[key] = 1;
+    }
+  }
+  return countedArrayToObj;
+};
+
 // Uncomment these to check your work!
-// function evenOdd(n) {
-//   if (n % 2 === 0) return 'even';
-//   else return 'odd';
-// }
-// const nums = [1, 2, 3, 4, 5];
-// console.log(countBy(nums, evenOdd)); // should log: { odd: 3, even: 2 }
+function evenOdd(n) {
+  if (n % 2 === 0) return 'even';
+  else return 'odd';
+}
+const nums = [1, 2, 3, 4, 5];
+console.log(countBy(nums, evenOdd)); // should log: { odd: 3, even: 2 }
