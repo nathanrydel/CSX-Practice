0c9c7541a7be871a605f3a9f4d02e1409c623cd0
Nathan Rydel <nathanrydel@gmail.com>
2023-04-03 22:31:40 -0700

    objOfMatches === complete

--- Callbacks & Higher-order Functions/objOfMatches.js
+++ Callbacks & Higher-order Functions/objOfMatches.js
@@ -2,8 +2,28 @@
 
 // ADD CODE HERE
 
+// input: array1, array2, callback
+// output: obj
+
+const objOfMatches = (array1, array2, callback) => {
+  // declare variable to store output
+  const updatedObj = {};
+  // Loop thru elements of array1
+  for (let i = 0; i < array1.length; i++) {
+    // check if element of array 1 using callback is strictly equal to array2 element at same index
+    if (callback(array1[i]) === array2[i]) {
+      // if match, element from array1 = key and element from array 2 = value
+      updatedObj[array1[i]] = array2[i];
+    }
+  }
+  // return output
+  return updatedObj;
+};
+
 // Uncomment these to check your work!
-// const arr1 = ['hi', 'howdy', 'bye', 'later', 'hello'];
-// const arr2 = ['HI', 'Howdy', 'BYE', 'later', 'HELLO'];
-// function uppercaser(str) { return str.toUpperCase(); }
-// console.log(objOfMatches(arr1, arr2, uppercaser)); // should log: { hi: 'HI', bye: 'BYE', hello: 'HELLO' }
+const arr1 = ['hi', 'howdy', 'bye', 'later', 'hello'];
+const arr2 = ['HI', 'Howdy', 'BYE', 'later', 'HELLO'];
+function uppercaser(str) {
+  return str.toUpperCase();
+}
+console.log(objOfMatches(arr1, arr2, uppercaser)); // should log: { hi: 'HI', bye: 'BYE', hello: 'HELLO' }
