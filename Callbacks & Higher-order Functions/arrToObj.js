00b998f55756ae4f08c36633b952eee50e8462d4
Nathan Rydel <nathanrydel@gmail.com>
2023-04-25 17:44:28 -0700

    used arrow function

--- Callbacks & Higher-order Functions/arrToObj.js
+++ Callbacks & Higher-order Functions/arrToObj.js
@@ -1,6 +1,6 @@
 // Add code to the function arrToObj in the place marked "ADD CODE HERE" in order to achieve the desired console logs. arrToObj should return an object that has elements from the passed-in array as keys, and the outputs from the callback (when those elements are passed in) as the corresponding values.
 
-function arrToObj(array, callback) {
+const arrToObj = (array, callback) => {
   // ADD CODE HERE
   let updatedObj = {};
   // iterate thru array
@@ -9,7 +9,7 @@ function arrToObj(array, callback) {
     updatedObj[array[i]] = callback(array[i]);
   }
   return updatedObj;
-}
+};
 
 // Uncomment these to check your work!
 const arrOfStrings = ['beer', 'glue'];
