00b2962a35a065715f44d7b33b9750a272c7641b
Nathan Rydel <nathanrydel@gmail.com>
2023-04-26 10:56:59 -0700

    scoping learning complete

--- Closure, Scope & Execution Context/scoping.js
+++ Closure, Scope & Execution Context/scoping.js
@@ -15,9 +15,9 @@ const jasCounter = outer();
 // Uncomment each of these lines one by one.
 // Before your do, guess what will be logged from each function call.
 
-// willCounter();
-// willCounter();
-// willCounter();
+willCounter();
+willCounter();
+willCounter();
 
-// jasCounter();
-// willCounter();
+jasCounter();
+willCounter();
