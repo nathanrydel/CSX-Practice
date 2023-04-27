ff37f2ef97f468fc4802cf8a044e44f3f4b3dae9
Nathan Rydel <nathanrydel@gmail.com>
2023-04-27 15:39:45 -0700

    hobbyTracker === complete

--- Closure, Scope & Execution Context/hobbyTracker.js
+++ Closure, Scope & Execution Context/hobbyTracker.js
@@ -13,13 +13,31 @@
 // updateHobbies();
 // // 'tracker has been reset!'
 
-function hobbyTracker(hobbies) {}
+// Create a function hobbyTracker that takes an array of hobbies
+function hobbyTracker(hobbies) {
+  // create an empty cache object
+  const cachedHobbies = {};
+  // iterate thru hobbies and add element of array as key and set to 0
+  hobbies.forEach((hobby) => (cachedHobbies[hobby] = 0));
+  // return a function that takes the hobby: string and a numOfHours: integer
+  return function (hobby, numOfHours) {
+    // check if empty arguments when function invoked
+    if (arguments.length === 0) {
+      // use Object.keys to check all the keys of cachedHobbies and change each value at key to 0
+      Object.keys(cachedHobbies).forEach((key) => (cachedHobbies[key] = 0));
+      // return 'tracker has been reset'
+      return 'tracker has been reset!';
+    }
+    cachedHobbies[hobby] += numOfHours;
+    return cachedHobbies;
+  };
+}
 
 // Uncomment the code below to check your code:
-// const updateHobbies = hobbyTracker(['yoga', 'baking', 'piano']);
-// updateHobbies('yoga', 2);
-// updateHobbies('baking', 4);
-// updateHobbies('yoga', 1);
-// console.log(updateHobbies('piano', 2)); // --> { yoga: 3, baking: 4, piano: 2 }
-// console.log(updateHobbies()); // --> 'tracker has been reset!'
-// console.log(updateHobbies('baking', 1)); // --> { yoga: 0, baking: 1, piano: 0}
+const updateHobbies = hobbyTracker(['yoga', 'baking', 'piano']);
+updateHobbies('yoga', 2);
+updateHobbies('baking', 4);
+updateHobbies('yoga', 1);
+console.log(updateHobbies('piano', 2)); // --> { yoga: 3, baking: 4, piano: 2 }
+console.log(updateHobbies()); // --> 'tracker has been reset!'
+console.log(updateHobbies('baking', 1)); // --> { yoga: 0, baking: 1, piano: 0}
