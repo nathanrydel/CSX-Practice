543b349db18c2c6d163f3eeb6ec4c9c10b269801
Nathan Rydel <nathanrydel@gmail.com>
2023-04-27 11:53:41 -0700

    saveOutput === complete

--- Closure, Scope & Execution Context/saveOutput.js
+++ Closure, Scope & Execution Context/saveOutput.js
@@ -2,9 +2,34 @@
 
 // ADD CODE HERE
 
+// declare a function, saveOutput, that accepts callback and password:string
+function saveOutput(func, password) {
+  // declare an empty obj to store all invocations
+  const cache = {};
+  // declare and return a function that behaves like callback with one argument
+  return function (argument) {
+    // check if argument === password
+    if (argument === password) {
+      // return cache obj
+      return cache;
+    } else {
+      // perform saveOutput func on argument
+      // save arg as key
+      cache[argument] = func(argument);
+      // return output value
+      return cache[argument];
+    }
+  };
+}
+
 // Uncomment these to check your work!
-// const multiplyBy2 = function(num) { return num * 2; };
-// const multBy2AndLog = saveOutput(multiplyBy2, 'boo');
-// console.log(multBy2AndLog(2)); // should log: 4
-// console.log(multBy2AndLog(9)); // should log: 18
-// console.log(multBy2AndLog('boo')); // should log: { 2: 4, 9: 18 }
+const multiplyBy2 = function (num) {
+  return num * 2;
+};
+const multBy2AndLog = saveOutput(multiplyBy2, 'boo');
+console.log(multBy2AndLog(2)); // should log: 4
+console.log(multBy2AndLog(9)); // should log: 18
+console.log(multBy2AndLog(11)); // should log: 18
+console.log(multBy2AndLog(14)); // should log: 18
+console.log(multBy2AndLog(144)); // should log: 18
+console.log(multBy2AndLog('boo')); // should log: { 2: 4, 9: 18 }
