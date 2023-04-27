317b835e24c4e60cff9f88cedd552320ac810892
Nathan Rydel <nathanrydel@gmail.com>
2023-04-27 11:30:23 -0700

    delay === complete

--- Closure, Scope & Execution Context/delay.js
+++ Closure, Scope & Execution Context/delay.js
@@ -2,9 +2,18 @@
 
 // ADD CODE HERE
 
+// declare a function, delay, that accepts a callback and waitTime: number
+function delay(callback, waitTime) {
+  // return a function
+  return function () {
+    // use setTimeout() to invoke callback after waitTime
+    return setTimeout(callback, waitTime);
+  };
+}
+
 // UNCOMMENT THE CODE BELOW TO TEST DELAY
-// let count = 0;
-// const delayedFunc = delay(() => count++, 1000);
-// delayedFunc();
-// console.log(count); 												 // should print '0'
-// setTimeout(() => console.log(count), 1000); // should print '1' after 1 second
+let count = 0;
+const delayedFunc = delay(() => count++, 1000);
+delayedFunc();
+console.log(count); // should print '0'
+setTimeout(() => console.log(count), 1000); // should print '1' after 1 second
