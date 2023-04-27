c98471534e552905189a2007e7eae1fb53792b8e
Nathan Rydel <nathanrydel@gmail.com>
2023-04-27 13:12:15 -0700

    defineFirstArg === complete

--- Closure, Scope & Execution Context/defineFirstArg.js
+++ Closure, Scope & Execution Context/defineFirstArg.js
@@ -2,7 +2,20 @@
 
 // ADD CODE HERE
 
+// create defineFirstArg function that accepts a function and an argument and returns a new function
+function defineFirstArg(callback, definedArgs) {
+  // passed in function will accept at least one argument(...args)
+  return function (...args) {
+    // new function invokes passed in function
+    // passed in arguments will be first arguments of of passed in function
+    // additional arguments needed by passed in function will be passed into return function
+    return callback(definedArgs, ...args);
+  };
+}
+
 // Uncomment these to check your work!
-// const subtract = function(big, small) { return big - small; };
-// const subFrom20 = defineFirstArg(subtract, 20);
-// console.log(subFrom20(5)); // should log: 15
+const subtract = function (big, small) {
+  return big - small;
+};
+const subFrom20 = defineFirstArg(subtract, 20);
+console.log(subFrom20(5)); // should log: 15
