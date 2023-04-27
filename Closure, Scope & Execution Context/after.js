a467eb74d0f02c6f856c58bdffd3dd69260c0c86
Nathan Rydel <nathanrydel@gmail.com>
2023-04-26 19:18:22 -0700

    after === complete

--- Closure, Scope & Execution Context/after.js
+++ Closure, Scope & Execution Context/after.js
@@ -2,12 +2,29 @@
 
 // ADD CODE HERE
 
+// create a function, after, that takes a number and callback
+function after(numberThreshold, callback) {
+  // create counter variable to track number of times invoked
+  let runCounter = 0;
+
+  // create inner function that returns function definition
+  return function (...args) {
+    // increment runCounter on every run
+    runCounter++;
+    // check if number of times run is equal to numberThreshold
+    if (runCounter >= numberThreshold) {
+      // if truthy, return callback on invocation
+      return callback.apply(this, args);
+    }
+  };
+}
+
 const called = function (string) {
   return 'hello ' + string;
 };
 const afterCalled = after(3, called);
 
 // UNCOMMENT THESE LINES TO TEST YOUR WORK
-// console.log(afterCalled('world')); // -> undefined is printed
-// console.log(afterCalled('world')); // -> undefined is printed
-// console.log(afterCalled('world')); // -> 'hello world' is printed
+console.log(afterCalled('world')); // -> undefined is printed
+console.log(afterCalled('world')); // -> undefined is printed
+console.log(afterCalled('world')); // -> 'hello world' is printed
