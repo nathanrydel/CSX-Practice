a6e7a593dba5294fbb6168f87fcadb55fc5479fa
nathanrydel <nathanrydel@gmail.com>
2023-05-01 11:27:19 -0700

    dateStamp === complete

--- Closure, Scope & Execution Context/dateStamp.js
+++ Closure, Scope & Execution Context/dateStamp.js
@@ -2,7 +2,25 @@
 
 // ADD CODE HERE
 
+// input: function
+// output: function that returns an obj
+
+// create a function, dateStamp, that accepts a function as a parameter
+function dateStamp(callback) {
+  // return a function that accepts any arguments from the passed in function
+  return function (...args) {
+    // create an object
+    const outputObj = {};
+    // create a property with the key "date" and a value of today's date not including the time
+    outputObj.date = new Date().toDateString();
+    // create a property with the key "output" and a value of the evaluated result of the passed in func & arguments
+    outputObj.output = callback(...args);
+    // return the object
+    return outputObj;
+  };
+}
+
 // Uncomment these to check your work!
-// const stampedMultBy2 = dateStamp(n => n * 2);
-// console.log(stampedMultBy2(4)); // should log: { date: (today's date), output: 8 }
-// console.log(stampedMultBy2(6)); // should log: { date: (today's date), output: 12 }
+const stampedMultBy2 = dateStamp((n) => n * 2);
+console.log(stampedMultBy2(4)); // should log: { date: (today's date), output: 8 }
+console.log(stampedMultBy2(6)); // should log: { date: (today's date), output: 12 }
