805b9083552c9e5a2daaa9ea3015c8facbc160b8
Nathan Rydel <nathanrydel@gmail.com>
2023-05-06 11:30:01 -0700

    personFromPersonStore === complete

--- Object Oriented Programming/personFromPersonStore.js
+++ Object Oriented Programming/personFromPersonStore.js
@@ -1,13 +1,21 @@
 // Create a function personFromPersonStore that takes as input a name and an age. When called, the function will create person objects using the Object.create method on the personStore object.
 
 const personStore = {
-  greet: function () {
+  greet() {
     console.log('hello');
   },
 };
 
 function personFromPersonStore(name, age) {
   // add code here
+  // declare person variable and assign to output of Object.create passing personStore as prototype
+  const person = Object.create(personStore);
+  // add name property
+  person.name = name;
+  // add age property
+  person.age = age;
+  // return person object
+  return person;
 }
 
 const sandra = personFromPersonStore('Sandra', 26);
