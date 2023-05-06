c02119dd0199ec354d3f5235baba3cfbdb49b0a4
Nathan Rydel <nathanrydel@gmail.com>
2023-05-06 13:58:28 -0700

    forEach === complete

--- Asynchronous JavaScript/forEach.js
+++ Asynchronous JavaScript/forEach.js
@@ -6,3 +6,25 @@
 // Write a function, delayLog, that takes as input a delayTime and an index, i. When invoked, the function should wait delayTime milliseconds before logging to the console, "printing element i" (with "i" replaced with the actual index passed in).
 
 // Putting it all together, run the delayLog function on each item of the delays array using the forEach function you created.
+
+// declare function forEach and pass in arr: array and cb: object/function
+function forEach(arr, cb) {
+  // iterate thru arr
+  for (let i = 0; i < arr.length; i++) {
+    // pass each element of arr to cb and its index
+    cb(arr[i], i);
+  }
+}
+
+// initialize variable, delays: array, and set to 200, 500, 0, 350
+const delays = [200, 500, 0, 350];
+
+// declare function delayLog and pass in delayTime and (index) i: number
+function delayLog(delayTime, i) {
+  // use setTimeout to console log desired string, with value of i
+  // pass in anon func with desired string and delayTime as interval
+  setTimeout(() => console.log(`printing element ${i}`), delayTime);
+}
+
+// invoke forEach with delays as arr arg and delayLog as cb arg
+forEach(delays, delayLog);
