9125c751b9c3cf08e4deb493704e1d2d2efc339e
Nathan Rydel <nathanrydel@gmail.com>
2023-05-06 11:24:48 -0700

    Chain Stores === complete

--- Object Oriented Programming/ChainStores.js
+++ Object Oriented Programming/ChainStores.js
@@ -8,23 +8,89 @@
 
 // The second method, closeStore, should take a single string as an argument. It should check to see if there is an instance of a Franchise with a city property that matches the input string in the locations array on the instance of Chain the method was called on. If so, it should remove the instance from the locations array, update the totalStores property and display the string 'name closed the store in city!' If not, log the string 'name doesn't have a store in city.'
 
+// initiatlize the class Chain
 class Chain {
   // add code here
+  // declare properties: name: string, totalStores: number, locations: array
+  name;
+  totalStores;
+  locations;
+
+  // declare constructor function that takes name as arg
+  constructor(name) {
+    // initialize name property with name arg
+    this.name = name;
+    // initialize totalStores property to 0
+    this.totalStores = 0;
+    // initialize locations property to empty array
+    this.locations = [];
+  }
 }
 
+// add openStore method to Chain prototype that takes owner and city args
+Chain.prototype.openStore = function (owner, city) {
+  // create new Franchise instance with owner and city args in newFranchise variable
+  const newFranchise = new Franchise(owner, city);
+  // add newFranchise instance to locations array
+  this.locations.push(newFranchise);
+  // increment totalStores
+  this.totalStores++;
+};
+
+// add closeStore method to Chain prototype that takes city as arg
+Chain.prototype.closeStore = function (city) {
+  // declare variable to track whether Franchise was removed
+  let removedFranchise = false;
+  // iterate over locations array with for loop
+  for (let i = 0; i < this.locations.length; i++) {
+    // check if location at index matches city argument
+    if (this.locations[i].city === city) {
+      // if yes, remove from array
+      this.locations.splice(i, 1);
+      // decrement totalStores
+      this.totalStores--;
+      // log desired string
+      console.log(`${this.name} closed the store in ${city}!`);
+      // set removedFranchise to true
+      removedFranchise = true;
+      // exit loop
+      break;
+    }
+  }
+  // check if Franchise not removed
+  if (!removedFranchise) {
+    // log desired string
+    console.log(`${this.name} doesn't have a store in ${city}.`);
+  }
+};
+
+// create Franchise Class
 class Franchise {
   //add code here
+  // declare owner: string, and city: string properties
+  owner;
+  city;
+
+  // declare constructor function with owner and city as args
+  constructor(owner, city) {
+    // initialize owner property to owner arg
+    this.owner = owner;
+    // iniitalize city property to city arg
+    this.city = city;
+  }
 }
 
 const buenoBell = new Chain('Bueno Bell');
 
 // Uncomment these lines to check your work!
-//buenoBell.openStore('Will', 'Phoenix');
-//buenoBell.openStore('Kyle', 'Austin');
-//buenoBell.openStore('Allison', 'Wichita');
+buenoBell.openStore('Will', 'Phoenix');
+buenoBell.openStore('Kyle', 'Austin');
+buenoBell.openStore('Allison', 'Wichita');
 
-//console.log(buenoBell.totalStores); //should log 3
-//console.log(buenoBell.locations[0].owner) //should log 'Will'
+console.log(buenoBell['locations']);
 
-//buenoBell.closeStore('Austin'); // Should log 'Bueno Bell closed the store in Austin.'
-//buenoBell.closeStore('Maui'); // Should log 'Bueno Bell doesn't have a store in Maui.'
+console.log(buenoBell.totalStores); //should log 3
+console.log(buenoBell.locations[0].owner); //should log 'Will'
+
+buenoBell.closeStore('Austin'); // Should log 'Bueno Bell closed the store in Austin.'
+buenoBell.closeStore('Maui'); // Should log 'Bueno Bell doesn't have a store in Maui.'
