7802b74c47b57233234c5823c1f6b154f2ab78e0
Nathan Rydel <nathanrydel@gmail.com>
2023-05-06 14:06:05 -0700

    ajaxSimulate === complete

--- Asynchronous JavaScript/ajaxSimulate.js
+++ Asynchronous JavaScript/ajaxSimulate.js
@@ -8,3 +8,27 @@
 // Invoke the ajaxSimulate function with an id of 1 and the storeData function as the callback. Immediately after, log to the console the value of dataReceived. What do you expect it to be?
 
 // Without changing anything else, copy-paste the console.log statement somewhere where it will log with the info we need.
+
+// initialize dataReceived variable
+let dataReceived;
+
+// declare function ajaxSimulate and pass in id: number and callback
+function ajaxSimulate(id, callback) {
+  // initialize variable database: array and set to ['Aaron', 'Barbara', 'Chris'];
+  const database = ['Aaron', 'Barbara', 'Chris'];
+  // set a timer that will pass element of database that matches id to callback function
+  setTimeout(() => {
+    callback(database[id]);
+  }, 0);
+  // set interval to 0ms
+}
+
+// declare function, storeData, takes data as input and assigns it to dataReceived
+function storeData(data) {
+  dataReceived = data;
+  // log dataReceived here so that it is executed in correct order
+  console.log(dataReceived);
+}
+
+// invoke ajaxSimulate(1, storeData);
+ajaxSimulate(1, storeData);
