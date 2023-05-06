33681458331c290e492293c2e9e072e7c4510668
Nathan Rydel <nathanrydel@gmail.com>
2023-05-06 12:41:03 -0700

    inventory === complete

--- Object Oriented Programming/inventory.js
+++ Object Oriented Programming/inventory.js
@@ -10,15 +10,72 @@
 
 function Inventory(item, price) {
   // add code here
+  // use helper function, createItemObject, to create an object with key set to item
+  // and have the evaluated result of invoking createItemObject on price be value
+  this[item] = createItemObject(price);
+}
+
+// declare a method, addItem, that takes item: string and price: number on the proto of Inventory
+Inventory.prototype.addItem = function (item, price) {
+  // check if the proto object has item as a key
+  if (this.hasOwnProperty(item)) {
+    // if so, increment item quantity and set price to input price
+    this[item].quantity++;
+    this[item].price = price;
+  } else {
+    // if not found, create the item, using createItemObject and set the price
+    this[item] = createItemObject(price);
+  }
+};
+
+// declare a method, deleteItem, that takes item: string on the proto of Inventory
+Inventory.prototype.deleteItem = function (item) {
+  // check if the prototypal object has the item as a key
+  if (this.hasOwnProperty(item)) {
+    // check the quantity of the item
+    if (this[item].quantity > 0) {
+      // if truthy, decrement quantity and return desired string
+      this[item].quantity--;
+      return 'Deleted';
+    } else {
+      // if falsy, return desired string
+      return 'Nothing to delete';
+    }
+    // if item doesn't exist, return Item is not in inventory
+  } else {
+    return 'Item is not in inventory';
+  }
+};
+
+// declare a method, checkItem, that takes an item: string that checks if the item exists on Inventory proto
+Inventory.prototype.checkItem = function (item) {
+  //check if prototypal object has item as a property
+  if (this.hasOwnProperty(item)) {
+    // if so, return item
+    return this[item];
+  } else {
+    // if not, return desired string
+    return 'Item is not in inventory';
+  }
+};
+
+// declaring Helper function, createItemObject, that takes in price: number
+function createItemObject(price) {
+  // return an object with a property(key) price and value to input price
+  // set quantity key to 1
+  return {
+    price: price,
+    quantity: 1,
+  };
 }
 
 const myInventory = new Inventory('cucumber', 2);
 
 // Uncomment these lines to check your work!
-// myInventory.addItem('carrot', 1);
-// console.log(myInventory.checkItem('cucumber')); // Logs: { price: 2, quantity: 1 }
-// myInventory.addItem('cucumber', 3);
-// console.log(myInventory.deleteItem('carrot')); // Logs: 'Deleted'
-// console.log(myInventory.deleteItem('carrot')); // Logs: 'Nothing to delete'
-// console.log(myInventory); // Logs: { cucumber: { price: 3, quantity: 2 }, carrot: { price: 1, quantity: 0 } }
-// console.log(myInventory.checkItem('radish')); // Logs: 'Item is not in inventory'
+myInventory.addItem('carrot', 1);
+console.log(myInventory.checkItem('cucumber')); // Logs: { price: 2, quantity: 1 }
+myInventory.addItem('cucumber', 3);
+console.log(myInventory.deleteItem('carrot')); // Logs: 'Deleted'
+console.log(myInventory.deleteItem('carrot')); // Logs: 'Nothing to delete'
+console.log(myInventory); // Logs: { cucumber: { price: 3, quantity: 2 }, carrot: { price: 1, quantity: 0 } }
+console.log(myInventory.checkItem('radish')); // Logs: 'Item is not in inventory'
