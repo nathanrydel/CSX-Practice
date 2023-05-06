897c86ce899eb5f825b26cc8b829e16ea2002fad
Nathan Rydel <nathanrydel@gmail.com>
2023-05-06 12:18:07 -0700

    PersonConstructor === complete

--- Object Oriented Programming/PersonConstructor.js
+++ Object Oriented Programming/PersonConstructor.js
@@ -2,9 +2,12 @@
 
 function PersonConstructor() {
   // add code here
+  this.greet = function () {
+    console.log('hello');
+  };
 }
 
 const simon = new PersonConstructor();
 
 // Uncomment this line to check your work!
-// simon.greet(); // -> Logs 'hello'
+simon.greet(); // -> Logs 'hello'
