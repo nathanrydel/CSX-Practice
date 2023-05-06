5b9a7a9575b0a968978b2cc5105e127df05b6f81
Nathan Rydel <nathanrydel@gmail.com>
2023-05-06 12:51:03 -0700

    DeveloperClass === complete

--- Object Oriented Programming/DeveloperClass.js
+++ Object Oriented Programming/DeveloperClass.js
@@ -11,9 +11,28 @@ class PersonClass {
 }
 
 // add code here
+// initialize class DeveloperClass that extends PersonClass
+class DeveloperClass extends PersonClass {
+  // delcare constructor that takes name: string
+  constructor(name) {
+    // initialize DeveloperClass with all properties of PersonClass using super() method
+    super(name);
+  }
+  // delcare method, introduce, in DeveloperClass
+  introduce() {
+    // console log desired string making sure it works for any instance
+    console.log(`Hello World, my name is ${this.name}`);
+  }
+}
 
 const thai = new DeveloperClass('Thai', 32);
+const sergei = new DeveloperClass('Sergei', 43);
 
 // Uncomment these lines to check your work!
-// console.log(thai.name); // -> Logs 'Thai'
-// thai.introduce(); // -> Logs 'Hello World, my name is Thai'
+console.log(thai.name); // -> Logs 'Thai'
+thai.introduce(); // -> Logs 'Hello World, my name is Thai'
+thai.greet(); // Logs 'hello'
+
+console.log(sergei.name); // Logs 'Sergei'
+sergei.introduce(); // Logs 'Hello World, my name is Sergei'
+sergei.greet(); // Logs 'hello'
