bfd44da95a723b4107a4c8b7e3480401279274fc
Nathan Rydel <nathanrydel@gmail.com>
2023-05-06 12:56:08 -0700

    Async JavaScript Module ROEs

---
+++ Asynchronous JavaScript/limitedInterval.js
@@ -0,0 +1,7 @@
+// Challenge: limitedInterval
+// Write a function called limitedInterval that accepts as arguments in this order -
+
+// callback function
+// wait time in milliseconds
+// limit time in milliseconds.
+// limitedInterval should run the callback once every wait milliseconds, up to limit milliseconds, and then stop.
