80e4a12a1ff478e8e01ad05fe735fd6756a2be7c
Nathan Rydel <nathanrydel@gmail.com>
2023-05-06 21:48:29 -0700

    runInOrder === complete

--- Asynchronous JavaScript/runInOrder.js
+++ Asynchronous JavaScript/runInOrder.js
@@ -5,7 +5,6 @@
 // an array of numbers representing times in milliseconds
 // runInOrder should execute the functions in order, with the corresponding numbers in milliseconds being the time to wait from the previous invocation. For example -
 
-/*
 function sayHi() {
   console.log('hi');
 }
@@ -16,6 +15,43 @@ function sayHowdy() {
   console.log('howdy');
 }
 
+/*
 // runInOrder([sayHi, sayBye, sayHowdy], [300, 600, 200]);
  should log: 'hi' (after 300 ms) 'bye' (600 ms after 'hi') 'howdy' (200 ms after 'bye') 
  */
+// declare function, runInOrder, that accepts funcs: array and times: array
+function runInOrder(funcs, times) {
+  // declare index variable to keep place
+  let index = 0;
+  // declare inner function, executeNext, to move thru funcs array
+  function executeNext() {
+    // if current index is less than funcs.length
+    if (index < funcs.length) {
+      // invoke setTimeout
+      setTimeout(() => {
+        // invoke funcs callback at current index
+        funcs[index]();
+        // increment index
+        index++;
+        // invoke executeNext recursively
+        executeNext();
+        // use times at index for interval
+      }, times[index]);
+    }
+  }
+  // invoke inner function to execute functions in order
+  executeNext();
+}
+
+// declare async function, runInOrder, that accepts functionsArr: array and intervalsArr: array as arguments
+async function asyncRunInOrder(functionsArr, intervalsArr) {
+  // iterate thru functionsArr
+  for (let i = 0; i < functionsArr.length; i++) {
+    // create a new Promise that resolves after corresponding intervalsArr[i] index
+    await new Promise((resolve) => setTimeout(resolve, intervalsArr[i]));
+    // wait for the Promise to resolve, then execute current function
+    functionsArr[i]();
+  }
+}
+
+runInOrder([sayHi, sayBye, sayHowdy], [300, 600, 200]); // should log: 'hi' (after 300 ms) 'bye' (600 ms after 'hi') 'howdy' (200 ms after 'bye')
